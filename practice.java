f8581355cd16be1e2dbb1b381c0d122aed0f4f2d
Shunsuke135 <shun.135.2109@gmail.com>
2021-04-06 13:11:05 +0000

    Java practice2 finish

--- practice.java
+++
@@ -1,8 +0,0 @@
-BufferedReader br = new BufferedReader(
-                        new InputStreamReader( System.in ) );
-
-int x = Integer.parseInt( br.readLine() );
-int y = Integer.parseInt( br.readLine() );
-
-if( x > y )
-    System.out.println( "xはyより大きい。" );
---
+++ practice2.java
@@ -0,0 +1,10 @@
+public class practice2 {
+  public static void main( String[] args )
+  {
+      int x;
+
+      x = 11;
+
+      System.out.println( "x=" + x );
+  } 
+}
