20b70b0148071eb812e7cdc969cae3f3da51a227
Shunsuke135 <shun.135.2109@gmail.com>
2021-04-21 13:13:18 +0000

    Java practice40 finish

---
+++ Java.2/practice29.java
@@ -0,0 +1,16 @@
+public class practice29 {
+  public static void main(String[] args){
+    int sushi = 3;
+    switch (sushi){
+      case 1:
+      System.out.println("まぐろ");
+      break;
+      case 2:
+      System.out.println("えび");
+      break;
+      case 3:
+      System.out.println("こはだ");
+      break;
+    }
+  }
+}
---
+++ Java.2/practice31.java
@@ -0,0 +1,8 @@
+public class practice31 {
+  public static void main(String[] args){
+    int san = 3;
+    for (int x = 1; x <= 9; x++){
+      System.out.println(san * x);
+    }
+  }
+}
